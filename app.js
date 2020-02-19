66cbbe4195ec8ca8431075fceaf9cf287a8a3474
vikrantkumar158 <vikrant171891.cse@chitkara.edu.in>
2020-02-19 23:05:27 +0530

    update

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-server.listen(3000, function () {
+server.listen(process.env.PORT||3000, function () {
    var host = server.address().address;
    var port = server.address().port;
    console.log('Example app listening at http://%s:%s', host, port);
