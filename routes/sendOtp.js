14eedcfca822501ea7a71c8f23f17152d6652aa2
vikrantkumar158 <vikrant171891.cse@chitkara.edu.in>
2020-02-23 10:26:55 +0530

    Going live

--- routes/sendOtp.js
+++ routes/sendOtp.js
@@ -12,13 +12,12 @@ module.exports = (app)=>{
 		console.log(process.env.emailID);
 		var x=generate.generateOtp();
 		var y=bcrypt.encrypt(x);
-		var mailOptions={
+		otp.send({
 			from: process.env.emailID,
 			to: req.body.email,
 			subject: 'Email Verification',
 			html: 'Your OTP for Email Verification is <b>'+x+'</b>'
-		};
-		otp.send(mailOptions,(err,data)=>{
+		},(err,data)=>{
 			if(err)
 				res.send(err);
 			res.render('verifyOtp.ejs',{email:req.body.email,hash:y});
@@ -28,13 +27,12 @@ module.exports = (app)=>{
 	app.post('/api/send',(req,res)=>{
 		var x=generate.generateOtp();
 		var y=bcrypt.encrypt(x);
-		var mailOptions={
+		otp.send({
 			from: process.env.emailID,
 			to: req.body.email,
 			subject: 'Email Verification',
 			html: 'Your OTP for Email Verification is <b>'+x+'</b>'
-		};
-		otp.send(mailOptions,(err,data)=>{
+		},(err,data)=>{
 			if(err)
 				res.send(err);
 			res.send(y);
