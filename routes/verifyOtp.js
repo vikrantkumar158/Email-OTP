1122a1ca06b73b00810571a15d359a4c26469b55
vikrantkumar158 <vikrant171891.cse@chitkara.edu.in>
2020-04-18 16:37:22 +0530

    v2

--- routes/verifyOtp.js
+++ routes/verifyOtp.js
@@ -22,4 +22,22 @@ module.exports = (app)=>
 				res.send("Failure. Kindly check again.");
 		})
 	});
+	
+	app.post('/api/verify',(req,res)=>{
+		otp.match(req.body.email,req.body.otp,(err,data)=>{
+			if(err)
+				res.send(err);
+			if(data==undefined||data==false)
+				res.send("Failure");
+			else if(data==true)
+			{
+				otp.remove(req.body.email,(error,dataa)=>{
+					if(error)
+						res.send(error);
+					else
+						res.send("Success");
+				})
+			}
+		})
+	});
 }
